f0da7a53b28bdf63f3cf6df58a75823e3d757f34
fadly-agani <39073633+fadly-agani@users.noreply.github.com>
2018-05-08 12:33:01 +0900

    Initial commit of main.js

---
+++ main.js
@@ -0,0 +1,15 @@
+function check(form){
+	
+	var user = document.getElementById('username');
+	var pass = document.getElementById('password');
+	
+	var coruser= "test";
+	var corpass= "123";
+ 
+ if(form.userid.value == coruser){
+	 if(form.pass.value == corpass){
+		 window.open('https://www.youtube.com/c/dewclarke?sub_confirmation=1')
+	 } else {alert("Incorect Password!!!")}
+  }
+ else{window.alert("Incorect Username or Create new account!!!")}
+}
